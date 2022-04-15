ebcbd183af49450ed531adaeaa3df32b3d29167b
= <quds.fenyx@gmail.com>
2022-04-15 17:33:38 -0400

    amplify added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to V2!
+          Welcome to V3!
         </p>
       </header>
     </div>
